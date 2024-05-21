a5f71f9469fdd01afcdd6539ca9de82d4915b514
Alexey Zaytsev <alex.rv9rh@gmail.com>
2024-05-21 05:45:24 +0500

    Refactoring

--- block.py
+++ block.py
@@ -1,10 +1,10 @@
 
 class Block:
-	def __init__(self, tag: str, posX: int = 0, posY: int = 0):
+	def __init__(self, tag: str = "", x: int = 0, y: int = 0):
 		self.tag = tag
-		self.posX = posX
-		self.posY = posY
+		self.x = x
+		self.y = y
 
 	def __str__(self):
-		return f"{self.tag} {self.posX} {self.posY}"
+		return f"{self.tag} {self.x} {self.y}"
 
--- blocks.py
+++ blocks.py
@@ -1,6 +1,8 @@
 
-from Block import Block
+from block import Block
 
 
 b1 = Block("b1", 2, 5)
+b2 = Block()
 print(b1)
+print(b2)
