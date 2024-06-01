a4ae2c295af5317334d3f8f5e39508dc7da38f99
Alexey Zaytsev <alex.rv9rh@gmail.com>
2024-06-01 06:15:11 +0500

    Add classes: Coords, TwoCoordsArrays

---
+++ two_coords_arrays.py
@@ -0,0 +1,10 @@
+
+from typing import List
+from coords import Coords
+
+
+class TwoCoordsArrays:
+    def __init__(self, first: List[Coords], second: List[Coords]) -> None:
+        self.first = first
+        self.second = second
+
